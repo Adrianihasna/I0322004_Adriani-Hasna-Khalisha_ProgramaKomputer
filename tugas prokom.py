9d0ee91300b2b967ca4cc540208afa3882989fc5
Adrianihasna <adrianihsk24@gmail.com>
2023-03-13 18:43:17 +0700

    Excercise-01

---
+++ tugas prokom.py
@@ -0,0 +1,9 @@
+print("*"*55 + "\n")
+print("\n"+ "Nama = Adriani Hasna Khalisha")
+print("NIM = I0322004"+"\n")
+print("*"*55 + "\n")
+print("""
+    Nama saya Adriani Hasna Khalisha saya berasal dari Kota Bekasi. Bekasi dikenal sebagai planet lain karena cuacanya yang sangat panas dan memiliki sedikit pusat perbelanjaan(mall). Akan tetapi, saat saya tinggal di Solo untuk berkuliah meurut saya Kota Solo tidak kalah panasnya. Meskipun sering didiskrimanasi dan disebut planet lain, tapi Bekasi adalah kota yang nyaman untuk saya.  
+    
+
+""")
